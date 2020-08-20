fb6bc476c3e39261f714dc4455b3014077bc15f6
AbdAsh <abdash@users.noreply.github.com>
2020-08-20 09:11:12 +0300

    Update Bookmark.dart

--- lib/Bookmark.dart
+++ lib/Bookmark.dart
@@ -8,11 +8,12 @@ class Bookmark {
 
 
   factory Bookmark.fromJson(Map<String, dynamic> json) {
+    var imageNotFound = 'https://blog.rahulbhutani.com/wp-content/uploads/2020/05/Screenshot-2018-12-16-at-21.06.29.png';
 
     return Bookmark(
       title : json['title'],
       description: json['description'],
-      image: json['urlToImage'],
+      image: json['urlToImage'] != null ? json['urlToImage'] : imageNotFound,
     );
   }
 }
